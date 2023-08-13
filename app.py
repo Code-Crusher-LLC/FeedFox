a33ea6333f8b58194f484faeec84e511a205fc4a
tangym <tangym@codecrusher.llc>
2023-08-13 03:07:11 +0000

    Bug fix: recover web crawling

--- app.py
+++ app.py
@@ -37,80 +37,80 @@ if __name__ == "__main__":
 
     feeds = config["feeds"]
 
-    # browser = Browser()
-    # browser.start()
-    # bundles = {}
-    # for feed_id in tqdm.tqdm(feeds, desc="Fetching feeds"):
-    #     feed = feeds[feed_id]
-    #     logger.info(f"Processing feed: {feed_id} - {feed['title']}")
+    browser = Browser()
+    browser.start()
+    bundles = {}
+    for feed_id in tqdm.tqdm(feeds, desc="Fetching feeds"):
+        feed = feeds[feed_id]
+        logger.info(f"Processing feed: {feed_id} - {feed['title']}")
 
-    #     if feed["bundle"] is None:
-    #         feed["bundle"] = default["bundle"]
-    #     if type(feed["bundle"]) == str:
-    #         feed["bundle"] = [feed["bundle"]]
+        if feed["bundle"] is None:
+            feed["bundle"] = default["bundle"]
+        if type(feed["bundle"]) == str:
+            feed["bundle"] = [feed["bundle"]]
 
-    #     paths = feed["bundle"]
-    #     logger.debug(f"Feed {feed_id} will be bundled in {paths}")
-    #     for path in paths:
-    #         if path not in bundles:
-    #             bundles[path] = FeedGenerator()
-    #             logger.debug(f"Initialize feed to bundle {path}")
-    #         path = os.path.join(public_base_path, path)
-    #         if not os.path.exists(path):
-    #             os.makedirs(path)
+        paths = feed["bundle"]
+        logger.debug(f"Feed {feed_id} will be bundled in {paths}")
+        for path in paths:
+            if path not in bundles:
+                bundles[path] = FeedGenerator()
+                logger.debug(f"Initialize feed to bundle {path}")
+            path = os.path.join(public_base_path, path)
+            if not os.path.exists(path):
+                os.makedirs(path)
 
-    #     fg = FeedGenerator()
-    #     fg.id(feed_id)
-    #     fg.title(feed.get("title", feed_id))
-    #     fg.description(feed.get("description", default.get("description", feed_id)))
-    #     fg.updated(dateparser.parse("now").astimezone())
-    #     fg.image(url=feed.get("image", default.get("image", None)))
-    #     if type(feed["link"]) == str:
-    #         links = [feed["link"]]
-    #         fg.link(href=feed["link"], rel="alternate")
-    #     elif type(feed["link"]) == list:
-    #         links = feed["link"]
-    #         fg.link(href=feed["link"][0], rel="alternate")
-    #     else:
-    #         raise ValueError("Invalid feed link")
-    #     if type(feed["template"]) == str:
-    #         template = templates[feed["template"]]
-    #     else:
-    #         template = Parser(**feed["template"])
+        fg = FeedGenerator()
+        fg.id(feed_id)
+        fg.title(feed.get("title", feed_id))
+        fg.description(feed.get("description", default.get("description", feed_id)))
+        fg.updated(dateparser.parse("now").astimezone())
+        fg.image(url=feed.get("image", default.get("image", None)))
+        if type(feed["link"]) == str:
+            links = [feed["link"]]
+            fg.link(href=feed["link"], rel="alternate")
+        elif type(feed["link"]) == list:
+            links = feed["link"]
+            fg.link(href=feed["link"][0], rel="alternate")
+        else:
+            raise ValueError("Invalid feed link")
+        if type(feed["template"]) == str:
+            template = templates[feed["template"]]
+        else:
+            template = Parser(**feed["template"])
 
-    #     timeout = feed.get("timeout", default.get("timeout", 30))
-    #     for link in links:
-    #         logger.info(f"Fetching {link}")
-    #         try:
-    #             html = browser.fetch(link, template.wait_for, timeout=timeout)
-    #         except Exception as e:
-    #             logger.error(f"Error while fetching {link}: {e}")
-    #             logger.info("Skipping...")
-    #             continue
-    #         entries = template.parse(html)
-    #         for entry in entries:
-    #             fg.add_entry(entry.entry)
-    #             for path in paths:
-    #                 bundles[path].add_entry(entry.entry)
+        timeout = feed.get("timeout", default.get("timeout", 30))
+        for link in links:
+            logger.info(f"Fetching {link}")
+            try:
+                html = browser.fetch(link, template.wait_for, timeout=timeout)
+            except Exception as e:
+                logger.error(f"Error while fetching {link}: {e}")
+                logger.info("Skipping...")
+                continue
+            entries = template.parse(html)
+            for entry in entries:
+                fg.add_entry(entry.entry)
+                for path in paths:
+                    bundles[path].add_entry(entry.entry)
 
-    #     for path in paths:
-    #         path = os.path.join(public_base_path, path)
-    #         fg.atom_file(os.path.join(path, f"{fg.id()}.atom"))
-    #         fg.rss_file(os.path.join(path, f"{fg.id()}.rss"))
+        for path in paths:
+            path = os.path.join(public_base_path, path)
+            fg.atom_file(os.path.join(path, f"{fg.id()}.atom"))
+            fg.rss_file(os.path.join(path, f"{fg.id()}.rss"))
 
-    # for path in tqdm.tqdm(bundles, desc="Generating bundles"):
-    #     logger.info(f"Generating bundle {path}")
-    #     bundle = bundles[path]
-    #     bundle.id(f"{path}-all")
-    #     bundle.title(f"{path}")
-    #     bundle.description(f"{path}")
-    #     bundle.updated(dateparser.parse("now").astimezone())
-    #     bundle.link(href=f"{path}/all.atom", rel="self")
+    for path in tqdm.tqdm(bundles, desc="Generating bundles"):
+        logger.info(f"Generating bundle {path}")
+        bundle = bundles[path]
+        bundle.id(f"{path}-all")
+        bundle.title(f"{path}")
+        bundle.description(f"{path}")
+        bundle.updated(dateparser.parse("now").astimezone())
+        bundle.link(href=f"{path}/all.atom", rel="self")
 
-    #     path = os.path.join(public_base_path, path)
-    #     bundle.atom_file(os.path.join(path, "all.atom"))
-    #     bundle.rss_file(os.path.join(path, "all.rss"))
-    # browser.stop()
+        path = os.path.join(public_base_path, path)
+        bundle.atom_file(os.path.join(path, "all.atom"))
+        bundle.rss_file(os.path.join(path, "all.rss"))
+    browser.stop()
 
     # Generate `index.html` files for bundles and feeds indexing
     bundles = {}
